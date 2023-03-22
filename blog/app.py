06f1e37cf4414bd643f8e52bf8598ab41c50afbe
P.Baranov <100687223+MadJhey@users.noreply.github.com>
2023-03-22 21:27:55 +0300

    lesson 1

--- blog/app.py
+++ blog/app.py
@@ -7,14 +7,17 @@ from flask import Request
 # __name__ имя файла
 app = Flask(__name__)
 
+
 @app.route('/<int:num>')
 def index(num: int):
     return f'Hello {num}'
 
+
 @app.before_request
 def before_request():
     pass
+
+
 @app.errorhandler(404)
 def error_404(error):
     return f'error 404'
-
