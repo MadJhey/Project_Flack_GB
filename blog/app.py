32231b209c5d1a72302ff2b003c062b9fe60cafc
P.Baranov <100687223+MadJhey@users.noreply.github.com>
2023-03-22 21:26:16 +0300

    lesson 1

---
+++ blog/app.py
@@ -0,0 +1,20 @@
+from datetime import time
+
+import werkzeug
+from flask import Flask
+from flask import Request
+
+# __name__ имя файла
+app = Flask(__name__)
+
+@app.route('/<int:num>')
+def index(num: int):
+    return f'Hello {num}'
+
+@app.before_request
+def before_request():
+    pass
+@app.errorhandler(404)
+def error_404(error):
+    return f'error 404'
+
