5fed777a06d88e42efe78763cacde074ee41b76d
P.Baranov <100687223+MadJhey@users.noreply.github.com>
2023-04-17 20:45:39 +0300

    lesson_8

--- blog/extensions.py
+++ blog/extensions.py
@@ -1,3 +1,4 @@
+from flask_admin import Admin
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -7,3 +8,4 @@ login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
+admin = Admin(name='Admin panel', template_mode='bootstrap4', )
