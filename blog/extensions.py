1cf0abab7c53b1af58b723444b76b25e85b4a974
P.Baranov <100687223+MadJhey@users.noreply.github.com>
2023-04-22 20:47:59 +0300

    api

--- blog/extensions.py
+++ blog/extensions.py
@@ -1,4 +1,5 @@
 from flask_admin import Admin
+from flask_combo_jsonapi import Api
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -9,3 +10,4 @@ db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
 admin = Admin(name='Admin panel', template_mode='bootstrap4', )
+api = Api()
