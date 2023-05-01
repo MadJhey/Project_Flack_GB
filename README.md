722c1ec216c3b54653bc403fb946135f9c8a1ec4
roman <belyakov1981@mail.ru>
2023-05-01 21:17:50 +0300

    update flask_deploy

--- README.md
+++
@@ -1,2 +0,0 @@
-# Project_Flack_GB
-Flack GB project
--- blog/forms/auth.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, validators, PasswordField, SubmitField
-
-
-class AuthForm(FlaskForm):
-    username = StringField('User name', [validators.DataRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
-    submit = SubmitField('Login')
